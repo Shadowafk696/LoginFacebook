10672bac0687dfaacd300e21ecc4e52c5efa7438
Shadowafk696 <al02689180@gmail.com>
2017-09-28 19:44:51 -0500

    First Commit

--- app/src/main/java/com/school/login/loginfacebook/LoginActivity.java
+++ app/src/main/java/com/school/login/loginfacebook/LoginActivity.java
@@ -7,18 +7,26 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 
-    public class LoginActivity extends FragmentActivity {
+public class LoginActivity extends FragmentActivity {
         CallbackManager callbackManager;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             FacebookSdk.sdkInitialize(getApplicationContext());
+            AppEventsLogger.activateApp(this);
             setContentView(R.layout.activity_login);
+
+            LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+            loginButton.setReadPermissions("public_profile");
+            loginButton.setReadPermissions("email");
+            loginButton.setReadPermissions("user_birthday");
 
             callbackManager = CallbackManager.Factory.create();
             LoginManager.getInstance().registerCallback(callbackManager, callback);
